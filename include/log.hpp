c1de3180fa0226eb9c5fd6d2063193d9c700d181
xXJSONDeruloXx <danielhimebauch@gmail.com>
2025-07-05 12:33:35 -0400

    fallback for c build err

--- include/log.hpp
+++ include/log.hpp
@@ -1,8 +1,17 @@
 #ifndef LOG_HPP
 #define LOG_HPP
 
-#include <format>
 #include <iostream>
+#include <sstream>
+#include <string>
+
+// Check if std::format is available
+#if __has_include(<format>) && __cpp_lib_format >= 202110L
+#include <format>
+#define HAS_STD_FORMAT 1
+#else
+#define HAS_STD_FORMAT 0
+#endif
 
 namespace Log {
 
@@ -12,6 +21,7 @@ namespace Log {
     const std::string_view GRAY = "\033[1;90m";
     const std::string_view RESET = "\033[0m";
 
+#if HAS_STD_FORMAT
     template<typename... Args>
     void info(std::format_string<Args...> fmt, Args&&... args) {
         std::cerr << WHITE << std::format(fmt, std::forward<Args>(args)...) << RESET << '\n';
@@ -31,6 +41,67 @@ namespace Log {
     void debug(std::format_string<Args...> fmt, Args&&... args) {
         std::cerr << GRAY << std::format(fmt, std::forward<Args>(args)...) << RESET << '\n';
     }
+#else
+    // Simple replacement function for basic {} placeholders
+    template<typename T>
+    std::string simple_format_impl(std::string str, T&& value) {
+        size_t pos = str.find("{}");
+        if (pos != std::string::npos) {
+            std::ostringstream oss;
+            oss << std::forward<T>(value);
+            str.replace(pos, 2, oss.str());
+        }
+        return str;
+    }
+
+    template<typename T, typename... Args>
+    std::string simple_format_impl(std::string str, T&& value, Args&&... args) {
+        size_t pos = str.find("{}");
+        if (pos != std::string::npos) {
+            std::ostringstream oss;
+            oss << std::forward<T>(value);
+            str.replace(pos, 2, oss.str());
+        }
+        return simple_format_impl(str, std::forward<Args>(args)...);
+    }
+
+    // Fallback implementations for older compilers
+    template<typename... Args>
+    void info(const std::string& fmt, Args&&... args) {
+        if constexpr (sizeof...(Args) > 0) {
+            std::cerr << WHITE << simple_format_impl(fmt, std::forward<Args>(args)...) << RESET << '\n';
+        } else {
+            std::cerr << WHITE << fmt << RESET << '\n';
+        }
+    }
+
+    template<typename... Args>
+    void warn(const std::string& fmt, Args&&... args) {
+        if constexpr (sizeof...(Args) > 0) {
+            std::cerr << YELLOW << simple_format_impl(fmt, std::forward<Args>(args)...) << RESET << '\n';
+        } else {
+            std::cerr << YELLOW << fmt << RESET << '\n';
+        }
+    }
+
+    template<typename... Args>
+    void error(const std::string& fmt, Args&&... args) {
+        if constexpr (sizeof...(Args) > 0) {
+            std::cerr << RED << simple_format_impl(fmt, std::forward<Args>(args)...) << RESET << '\n';
+        } else {
+            std::cerr << RED << fmt << RESET << '\n';
+        }
+    }
+
+    template<typename... Args>
+    void debug(const std::string& fmt, Args&&... args) {
+        if constexpr (sizeof...(Args) > 0) {
+            std::cerr << GRAY << simple_format_impl(fmt, std::forward<Args>(args)...) << RESET << '\n';
+        } else {
+            std::cerr << GRAY << fmt << RESET << '\n';
+        }
+    }
+#endif
 
 }
 
