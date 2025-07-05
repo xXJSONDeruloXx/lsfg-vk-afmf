e4e19097016ebd28a2cf1cb6813dbf432c59cdf1
xXJSONDeruloXx <danielhimebauch@gmail.com>
2025-07-05 14:15:11 -0400

    start destubbing and integrating ffxsdk

--- src/afmf.cpp
+++ src/afmf.cpp
@@ -1,9 +1,12 @@
 #include <afmf.hpp>
 #include "log.hpp"
 
-// TODO: Enable once FidelityFX SDK is properly integrated
-// #include <FidelityFX/host/ffx_frameinterpolation.h>
-// #include <FidelityFX/host/backends/vk/ffx_vk.h>
+#ifdef FFX_SDK_AVAILABLE
+#include <FidelityFX/host/ffx_frameinterpolation.h>
+#include <FidelityFX/host/backends/vk/ffx_vk.h>
+#include <FidelityFX/host/ffx_interface.h>
+#include <vulkan/vulkan.h>
+#endif
 
 #include <unordered_map>
 #include <memory>
@@ -13,8 +16,11 @@ namespace AFMF {
 namespace {
 
 struct AFMFContext {
-    // TODO: Enable once FidelityFX SDK is integrated
-    // FfxFrameInterpolationContext context;
+#ifdef FFX_SDK_AVAILABLE
+    FfxFrameInterpolationContext ffxContext;
+    FfxInterface backendInterface;
+    bool ffxInitialized = false;
+#endif
     uint32_t width, height;
     std::vector<int> outputDescriptors;
     int input0, input1;
@@ -23,6 +29,13 @@ struct AFMFContext {
 std::unordered_map<int32_t, std::unique_ptr<AFMFContext>> contexts;
 int32_t nextContextId = 1;
 bool initialized = false;
+
+#ifdef FFX_SDK_AVAILABLE
+// Global backend state
+FfxDevice ffxDevice = nullptr;
+void* scratchBuffer = nullptr;
+size_t scratchBufferSize = 0;
+#endif
 
 } // anonymous namespace
 
@@ -40,8 +53,17 @@ void initialize() {
     
     Log::info("Initializing AFMF (AMD FidelityFX Motion Frames)");
     
-    // TODO: Initialize FidelityFX backend
-    // This will need to be implemented once we have the FidelityFX SDK integration
+#ifdef FFX_SDK_AVAILABLE
+    Log::info("FidelityFX SDK available - initializing native backend");
+    
+    // TODO: Get Vulkan device and physical device from our Vulkan hooks
+    // For now, we'll defer this until we have proper device access
+    // This will be implemented when we connect with the Vulkan hook system
+    
+    Log::info("FidelityFX backend initialization deferred until device creation");
+#else
+    Log::info("FidelityFX SDK not available - using stub implementation");
+#endif
     
     initialized = true;
     Log::info("AFMF initialized successfully");
@@ -63,11 +85,33 @@ int32_t createContext(uint32_t width, uint32_t height, int in0, int in1,
     context->input1 = in1;
     context->outputDescriptors = outN;
     
+#ifdef FFX_SDK_AVAILABLE
+    Log::debug("Preparing FidelityFX Frame Interpolation context");
+    
     // TODO: Create FidelityFX Frame Interpolation context
-    // FfxFrameInterpolationContextDescription desc = {};
-    // desc.maxRenderSize.width = width;
-    // desc.maxRenderSize.height = height;
-    // auto result = ffxFrameInterpolationContextCreate(&context->context, &desc);
+    // This requires Vulkan device access which will come from the hook system
+    /*
+    FfxFrameInterpolationContextDescription desc = {};
+    desc.maxRenderSize.width = width;
+    desc.maxRenderSize.height = height;
+    desc.displaySize.width = width;
+    desc.displaySize.height = height;
+    desc.backBufferFormat = FFX_SURFACE_FORMAT_R8G8B8A8_UNORM; // TODO: Get from swapchain
+    desc.backendInterface = backendInterface;
+    
+    FfxErrorCode result = ffxFrameInterpolationContextCreate(&context->ffxContext, &desc);
+    if (result == FFX_OK) {
+        context->ffxInitialized = true;
+        Log::info("FidelityFX Frame Interpolation context created successfully");
+    } else {
+        Log::error("Failed to create FidelityFX context: {}", static_cast<int>(result));
+    }
+    */
+    
+    Log::debug("FidelityFX context creation deferred until Vulkan device available");
+#else
+    Log::debug("Using stub AFMF context (FidelityFX SDK not available)");
+#endif
     
     int32_t id = nextContextId++;
     contexts[id] = std::move(context);
@@ -102,8 +146,17 @@ void deleteContext(int32_t id) {
     
     Log::info("Deleting AFMF context ID: {}", id);
     
-    // TODO: Destroy FidelityFX context
-    // ffxFrameInterpolationContextDestroy(&it->second->context);
+#ifdef FFX_SDK_AVAILABLE
+    auto& context = it->second;
+    if (context->ffxInitialized) {
+        FfxErrorCode result = ffxFrameInterpolationContextDestroy(&context->ffxContext);
+        if (result != FFX_OK) {
+            Log::error("Failed to destroy FidelityFX context: {}", static_cast<int>(result));
+        } else {
+            Log::debug("FidelityFX context destroyed successfully");
+        }
+    }
+#endif
     
     contexts.erase(it);
 }
@@ -118,12 +171,26 @@ void finalize() {
     // Clean up all remaining contexts
     for (auto& [id, context] : contexts) {
         Log::warn("Cleaning up remaining AFMF context ID: {}", id);
-        // TODO: Destroy FidelityFX context
-        // ffxFrameInterpolationContextDestroy(&context->context);
+#ifdef FFX_SDK_AVAILABLE
+        if (context->ffxInitialized) {
+            FfxErrorCode result = ffxFrameInterpolationContextDestroy(&context->ffxContext);
+            if (result != FFX_OK) {
+                Log::error("Failed to destroy FidelityFX context {}: {}", id, static_cast<int>(result));
+            }
+        }
+#endif
     }
     contexts.clear();
     
-    // TODO: Finalize FidelityFX backend
+#ifdef FFX_SDK_AVAILABLE
+    // Clean up global FidelityFX resources
+    if (scratchBuffer) {
+        free(scratchBuffer);
+        scratchBuffer = nullptr;
+        scratchBufferSize = 0;
+    }
+    Log::debug("FidelityFX backend resources cleaned up");
+#endif
     
     initialized = false;
     Log::info("AFMF finalized");
